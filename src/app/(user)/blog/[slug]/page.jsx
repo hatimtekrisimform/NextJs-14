3c96b37f38deb007b65bb5cda9ae2c1e34af1f64
hatimtekrisimform <hatim@simformsolutions.com>
2024-03-18 22:41:01 +0530

    blog and blog slug ( dynamic blog )

--- src/app/(user)/blog/[slug]/page.jsx
+++ src/app/(user)/blog/[slug]/page.jsx
@@ -1,5 +1,58 @@
-const DynamicBlog = () => {
-  return <div>Dynamic Blog</div>;
+import Image from "next/image";
+import styles from "./DynamicBlog.module.css";
+import PostUser from "../../../../components/postuser/PostUser";
+import { Suspense } from "react";
+
+export const generateMetadata = async ({ params }) => {
+  const { slug } = params;
+
+  const post = {
+    title:slug,
+    desc:slug
+  }
+
+  return {
+    title: post.title,
+    description: post.desc,
+  };
 };
 
-export default DynamicBlog
+const DynamicBlog = ({ params }) => {
+  const { slug } = params;
+
+  const post = {
+    img:"",
+    title:slug,
+    userId:"test id",
+    createdAt:"",
+    desc:slug
+  }
+  return (
+    <div className={styles.container}>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.desc}</div>
+      </div>
+    </div>
+  );
+};
+
+export default DynamicBlog;
--- src/app/(user)/blog/page.jsx
+++ src/app/(user)/blog/page.jsx
@@ -1,9 +1,44 @@
-const blogs = ()=>{
-    return(
-        <div>
-            blogs
-        </div>
-    )
-}
+import PostCard from "../../../components/postcard/PoastCard";
+import styles from "./blog.module.css";
 
-export default blogs
+const blogs = async () => {
+  const posts = [
+    {
+      img: "/contact.png",
+      createdAt: "",
+      title: "Test",
+      body: "Body",
+      slug: "test",
+    },
+    {
+        img: "/contact.png",
+      createdAt: "",
+      title: "Test 2",
+      body: "Body 2",
+      slug: "test2",
+    },
+    {
+        img: "/contact.png",
+      createdAt: "",
+      title: "Test 3",
+      body: "Body 3",
+      slug: "test3",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard post={posts[0]} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={posts[1]}  />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={posts[2]}  />
+      </div>
+     
+    </div>
+  );
+};
+
+export default blogs;

7fda235eed2d00704399578bc368b54ee0640f02
hatimtekrisimform <hatim@simformsolutions.com>
2024-05-22 13:11:53 +0530

    remove comments

--- src/lib/ConnectToDb.js
+++ src/lib/ConnectToDb.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-//const connectionString = `mongodb+srv://hatimtekri:sxofJlRbPmjVSOJW@cluster0.9k0ff36.mongodb.net/nextjs14?retryWrites=true&w=majority&appName=Cluster0`;
-//const connectionString = "mongodb+srv://hatim:yeomPj5EmxBh5ZeV@learn.zurrun3.mongodb.net/nextjs14?retryWrites=true&w=majority"
-
 const connectToDb = async () => {
   if (mongoose.connections[0].readyState) return;
 
